1d14be4e0ed0818f7420939320d5677bb5eaeade
Iurii Marymonskyi <yuriy.marimonsky@gmail.com>
2021-01-24 18:05:33 +0100

    Add dates support

--- inst/test.R
+++ inst/test.R
@@ -1,13 +1,14 @@
 # CREATE TABLE dbo.DemoTable (
 #   A INT NULL,
-#   B INT NULL
+#   B INT NULL,
+#   D DATE NULL
 # )
 
 library(bcp)
 
 conn = odbcDriverConnect('driver={SQL Server};server=DESKTOP-0U0OJS1\\SQLEXPRESS;database=test;trusted_connection=true')
 
-df = data.frame(A = 1:10, B = 1:10)
+df = data.frame(A = 1:10, B = 1:10, D = as.Date("2021-01-1"))
 
 bcp(conn, df, "dbo.DemoTable")
 
--- src/bcp.c
+++ src/bcp.c
@@ -5,6 +5,12 @@
 #include <Windows.h>
 #include <sql.h>
 #include <sqlext.h>
+#include <time.h>
+
+#define CHAR_TYPE 'C'
+#define DATE_TYPE 'D'
+#define REAL_TYPE 'R'
+#define INT_TYPE 'I'
 
 static inline SEXP attr(SEXP vec, SEXP name) {
     
@@ -43,6 +49,34 @@ void check_error(SQLRETURN r, char* msg, SQLHANDLE handle, SQLSMALLINT type) {
     Rf_error("bcp call failed\n");
 }
 
+int check_variable_class(const SEXP var_obj, const char *class_name) {
+
+    SEXP obj_classes  = getAttrib(var_obj, R_ClassSymbol);
+
+    if (!isNull(obj_classes)) {
+        for(int i = 0; i < length(obj_classes); i++) {
+            if (strcmp(CHAR(STRING_ELT(obj_classes, i)), class_name) == 0) {
+                return TRUE;
+            }
+        }
+    }
+
+    return FALSE;
+}
+
+void convert_to_sql_date(int r_date, DATE_STRUCT *sql_date) {
+
+    // convert days since epoch to seconds since epoch
+    time_t unix_time = r_date * 86400;
+
+    struct tm * ptm;
+    ptm = gmtime(&unix_time);
+    
+    sql_date->day = ptm->tm_mday;
+    sql_date->month = ptm->tm_mon + 1;
+    sql_date->year = ptm->tm_year + 1900;
+}
+
 void bcp(SQLHDBC dbc, SEXP r_data, const char* table_name, int chunk_size, int show_progress) {
 
     if (chunk_size < 1) {
@@ -78,9 +112,26 @@ void bcp(SQLHDBC dbc, SEXP r_data, const char* table_name, int chunk_size, int s
     SEXP col_names = attr(r_data, R_NamesSymbol);
 
     int char_col_len = 0;
+    int date_col_len = 0;
+    unsigned char col_types[col_len];
+
     for(int i = 0; i < col_len; i++) {
-        if (TYPEOF(VECTOR_ELT(r_data, i)) == STRSXP) {
-            char_col_len++;           
+        SEXP col_data = VECTOR_ELT(r_data, i);
+        if (TYPEOF(col_data) == STRSXP) {
+            char_col_len++;
+            col_types[i] = CHAR_TYPE;
+        } else if (TYPEOF(col_data) == REALSXP) {
+            if (check_variable_class(col_data, "Date")) {
+                col_types[i] = DATE_TYPE;
+                date_col_len++;
+            } else {
+                col_types[i] = REAL_TYPE;
+            }
+        } else if (TYPEOF(col_data) == INTSXP) {
+            col_types[i] = INT_TYPE;
+        } else {
+            const char* col_name = CHAR(STRING_ELT(col_names, i));
+            Rf_error("Could not load column %s - type not supported\n", col_name);
         }
     }
     
@@ -96,57 +147,64 @@ void bcp(SQLHDBC dbc, SEXP r_data, const char* table_name, int chunk_size, int s
         int row_volume = complete - start;
         
         char char_data[char_col_len][row_volume][256];
-        int char_data_index;
+        int char_data_index = 0;
+        
+        DATE_STRUCT date_data[date_col_len][row_volume];
+        int date_data_index = 0;
         
         for(int i = 0; i < col_len; i++) {
 
             int col_num = i + 1;
+
             SEXP column = VECTOR_ELT(r_data, i);
+            
+            memset(col_info[i], 0, sizeof(SQLLEN) * row_volume);
 
-            if (TYPEOF(column) == STRSXP) {
+            if (col_types[i] == CHAR_TYPE) {
+            
+                for(int j = start; j < complete; j++) {
+                    const char * d_val = CHAR(STRING_ELT(column, j));
+                    strcpy((char*) char_data[char_data_index][j - start], d_val);
+                }
 
-                    for(int j = start; j < complete; j++) {
-                        const char * d_val = CHAR(STRING_ELT(column, j));
-                        strcpy((char*) char_data[char_data_index][j - start], d_val);
+                res = SQLBindCol(stmt, col_num, SQL_C_CHAR, char_data[char_data_index][0], sizeof(char_data[char_data_index][0]), NULL);
+                check_error(res, "Bind column", stmt, SQL_HANDLE_STMT);
+                char_data_index++;
+
+            } else if (col_types[i] == REAL_TYPE || col_types[i] == DATE_TYPE) {
+            
+                double* num_data_array = REAL(column);
+
+                for(int j = start; j < complete; j++) {
+                    if (ISNA(num_data_array[j])) {
+                        col_info[i][j - start] = SQL_COLUMN_IGNORE;
                     }
+                }
 
-                    res = SQLBindCol(stmt, col_num, SQL_C_CHAR, char_data[char_data_index][0], sizeof(char_data[char_data_index][0]), NULL);
-                    check_error(res, "Bind column", stmt, SQL_HANDLE_STMT);
-                    char_data_index++;
-
-            } else if (TYPEOF(column) == INTSXP) {
-
-                    int* data_array = INTEGER(column);
-                    
-                    memset(col_info[i], 0, sizeof(SQLLEN) * row_volume);
-
+                if (col_types[i] == DATE_TYPE) {
                     for(int j = start; j < complete; j++) {
-                        if (data_array[j] == NA_INTEGER) {
-                            col_info[i][j - start] = SQL_COLUMN_IGNORE;
-                        }
+                        convert_to_sql_date(num_data_array[j], &date_data[date_data_index][j - start]);
                     }
+                    res = SQLBindCol(stmt, col_num, SQL_C_TYPE_DATE, &date_data[date_data_index++], sizeof(DATE_STRUCT), col_info[i]);                        
+                } else {
+                    res = SQLBindCol(stmt, col_num, SQL_C_DOUBLE, &num_data_array[start], sizeof(double), col_info[i]);
+                }
 
-                    res = SQLBindCol(stmt, col_num, SQL_C_LONG, &data_array[start], sizeof(int), col_info[i]);
-                    check_error(res, "Bind column", stmt, SQL_HANDLE_STMT);
-                
-            } else if (TYPEOF(column) == REALSXP) {
-                
-                    double* data_array = REAL(column);
-                    
-                    memset(col_info[i], 0, sizeof(SQLLEN) * row_volume);
+                check_error(res, "Bind column", stmt, SQL_HANDLE_STMT);
+                            
+            } else if (col_types[i] == INT_TYPE) {
+            
+                int* int_data_array = INTEGER(column);
 
-                    for(int j = start; j < complete; j++) {
-                        if (ISNA(data_array[j])) {
-                            col_info[i][j - start] = SQL_COLUMN_IGNORE;
-                        }
+                for(int j = start; j < complete; j++) {
+                    if (int_data_array[j] == NA_INTEGER) {
+                        col_info[i][j - start] = SQL_COLUMN_IGNORE;
                     }
-                
-                    res = SQLBindCol(stmt, col_num, SQL_C_DOUBLE, &data_array[start], sizeof(double), col_info[i]);
-                    check_error(res, "Bind column", stmt, SQL_HANDLE_STMT);
+                }
 
-            } else {
-                const char* col_name = CHAR(STRING_ELT(col_names, i));
-                Rf_error("Could not load column %s - type not supported\n", col_name);
+                res = SQLBindCol(stmt, col_num, SQL_C_LONG, &int_data_array[start], sizeof(int), col_info[i]);
+                check_error(res, "Bind column", stmt, SQL_HANDLE_STMT);
+                
             }
         }
 
@@ -172,21 +230,6 @@ void bcp(SQLHDBC dbc, SEXP r_data, const char* table_name, int chunk_size, int s
 
     // TODO: make sure that SQL handle is properly freed in case of errors
     SQLFreeHandle(SQL_HANDLE_STMT, stmt);
-}
-
-int check_variable_class(const SEXP var_obj, const char *class_name) {
-
-    SEXP obj_classes  = getAttrib(var_obj, R_ClassSymbol);
-
-    if (!isNull(obj_classes)) {
-        for(int i = 0; i < length(obj_classes); i++) {
-            if (strcmp(CHAR(STRING_ELT(obj_classes, i)), class_name) == 0) {
-                return TRUE;
-            }
-        }
-    }
-
-    return FALSE;
 }
 
 SEXP R_bcp(SEXP r_handle_ptr, SEXP r_data, SEXP r_table_name, SEXP r_chunk_size, SEXP r_show_progress) {
